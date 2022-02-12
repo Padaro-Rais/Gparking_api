e493b532643d80339d618053201f8f0e0c737c54
Padaro-Rais <wara.padaro@gmail.com>
2022-02-12 14:44:20 +0000

    ...

--- app/Http/Controllers/Auth/logMobileController.php
+++ app/Http/Controllers/Auth/logMobileController.php
@@ -19,18 +19,18 @@ class logMobileController extends Controller
      */
     public function login(Request $request)
     {
-        // $v = Validator::make($request->all(), [
-        //     'code' => 'required',
-        //     'matricule_ent' => 'required',
-        // ]);
+        $v = Validator::make($request->all(), [
+            'code' => 'required',
+            'matricule_ent' => 'required',
+        ]);
 
-        // if ($v->fails()) {
-        //     $args['error'] = true;
-        //     $args['validator'] = $v->errors();
-        //     return response()->json($v->errors(), 400);
-        // }
+        if ($v->fails()) {
+            $args['error'] = true;
+            $args['validator'] = $v->errors();
+            return response()->json($v->errors(), 400);
+        }
 
-        $agent = Agent::get();
+        $agent = Agent::where('code',$request->code)->where('entriprise_id',$request->matricule_ent)->get();
 
         return response()->json($agent);
 
